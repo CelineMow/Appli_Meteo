61eb2ad9839b431a7c94c4ae822aceba011da37d
Celine <celine.pro.morel@gmail.com>
2024-04-05 16:19:52 +0200

    function.js

--- function.js
+++ function.js
@@ -20,7 +20,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let panel = document.getElementById('panel');
 
     panel.addEventListener('click', function (event) {
-        console.log(event.target)
+        console.log(event.target, 'toto')
+
     });
 
 });
@@ -57,38 +58,6 @@ form.addEventListener('submit', (e) => {
     }
 });
 
-function jourDeLaSemaine(jour, mois, annee) {
-    const jourSemaine = [
-        "Dimanche",
-        "Lundi",
-        "Mardi",
-        "Mercredi",
-        "Jeudi",
-        "Vendredi",
-        "Samedi"
-    ];
-    return jourSemaine[new Date(`${annee}-${mois}-${jour}`).getDay()];
-}
-
-function getMonthName(monthNumber) {
-    const months = [
-        "Janvier", "Février", "Mars", "Avril", "Mai", "Juin",
-        "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
-    ];
-    return months[monthNumber];
-}
-
-function showDate() {
-    let date = new Date()
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    if (h < 10) { h = '0' + h; }
-    if (m < 10) { m = '0' + m; }
-    if (s < 10) { s = '0' + s; }
-    let time = h + ':' + m //+ ':' + s
-    heureOutPut.innerHTML = time;
-}
 
 function fetchDonneesMeteo() {
     console.log('pourquoi 2', villeInput)
@@ -184,24 +153,6 @@ function showPosition(position) {
     fetchDonneesMeteoByCoords(latitude, longitude);
 }
 
-function showError(error) {
-    console.error("Erreur lors de la récupération de la position :", error);
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            alert("L'utilisateur a refusé la demande de géolocalisation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("L'emplacement de l'utilisateur n'est pas disponible.");
-            break;
-        case error.TIMEOUT:
-            alert("La demande de géolocalisation de l'utilisateur a expiré.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("Une erreur inconnue s'est produite lors de la récupération de la géolocalisation.");
-            break;
-    }
-}
-
 function fetchDonneesMeteoByCoords(latitude, longitude) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=015b55e1c669b4fcb83d93a285b92ab1&units=metric`)
         .then(response => response.json())
@@ -280,13 +231,15 @@ function addCity(cityName) {
     console.log("Ajout de la ville :", cityName);
     const cityList = document.getElementById('cityList');
     const cityElement = document.createElement('li');
+
+    cityElement.innerHTML = cityName
     cityElement.classList.add('ville');
-    cityElement.textContent = cityName;
+    // cityElement.textContent = cityName;
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Supprimer';
     deleteBtn.classList.add('delete-btn');
     deleteBtn.addEventListener('click', () => deleteCity(cityName));
-    deleteBtn.addEventListener('click', () => console.log(cityName));
+
     cityElement.appendChild(deleteBtn);
     cityList.appendChild(cityElement);
 }
